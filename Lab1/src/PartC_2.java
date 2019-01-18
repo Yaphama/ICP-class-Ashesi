f7501f0f75d30fc52d7edf4cd77feb1c180de926
Pride Yaphama Mhango <35257659+Yaphama@users.noreply.github.com>
2019-01-18 19:46:23 +0000

    Update PartC_2.java

--- Lab1/src/PartC_2.java
+++ Lab1/src/PartC_2.java
@@ -1,12 +1,15 @@
 package labs;
 public class PartC_2 {
-	public static String reverse(String string) {
+	//Method that reverses any phrase sentence given to it.
+	public static String reverse(String string) { 
 		String newString = "";
 		
 		for(int x = string.length()-1; x>=0; x--) {
 			char character = string.charAt(x); 
 				newString = newString + character;
-			}
+			} 
+		//For loop that flips the the whole  sentence to 
+		
 		String[] myList = newString.split(" ");
 		newString = "";
 		for(int x = myList.length - 1; x>=0; x--) {
@@ -19,8 +22,8 @@ public class PartC_2 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String Pride = "SGNIHt era gnitseretni";
-		String newish = reverse(Pride);
+		String word = "sgniHt era gnitseretni";
+		String newish = reverse(word);
 		System.out.println(newish);
 	}
 
